959f01126ce97978dd221192a95dce7535d781f7
Smbat Sargsyan <smbat_s@instigatemobile.com>
2016-10-14 12:19:47 +0400

    Support of RN0.25+

--- StatelessForm.js
+++ StatelessForm.js
@@ -1,4 +1,5 @@
-import React, { Platform, Component, PropTypes, ScrollView, View } from 'react-native'
+import React, { Component, PropTypes } from 'react';
+import { Platform, ScrollView, View } from 'react-native'
 
 export default class StatelessForm extends Component {
   componentDidMount() {
